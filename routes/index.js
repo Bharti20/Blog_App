0d2e8d0f693bc0d9164bbf093557465f5c7dc078
Bharti kumari <bhartikumari20@navgurukul.org>
2021-08-21 07:34:28 +0530

    show alllikes/deslikes of posts

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,13 @@ const postLogInRoute = require('./postLoginRoute')
 const createPostsRoute = require('./postCreatePostsRoute')
 const getAllPostsRoute = require('./getAllPostRoute')
 const likeDeslikesRoute = require('./postLikeDeslikeRoute')
-
+const getLikesDeslikesRoute = require('./getLikesDeslikesRoute')
 
 Router.use('/signUp', postRegisterRoute)
 Router.use('/login', postLogInRoute)
 Router.use('/createPost', createPostsRoute)
 Router.use('/allPosts',getAllPostsRoute )
 Router.use('/likeDeslike', likeDeslikesRoute)
+Router.use('/showLikesDeslikes', getLikesDeslikesRoute)
 
 module.exports = Router
